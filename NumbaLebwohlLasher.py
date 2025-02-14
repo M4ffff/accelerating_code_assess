b293eda4300770d4cb299aab98c11712570d989e
Matthew Parker <matthewparker0105@yahoo.com>
2025-02-14 10:29:30 +0000

    used numba to speed up get_order function

--- NumbaLebwohlLasher.py
+++ NumbaLebwohlLasher.py
@@ -203,6 +203,18 @@ def all_energy(arr,nmax):
   
   
 #=======================================================================
+@jit(nopython=True)
+def calc_order(Qab, delta, arr, nmax):
+    lab = np.vstack((np.cos(arr),np.sin(arr),np.zeros_like(arr))).reshape(3,nmax,nmax)
+    for a in range(3):
+        for b in range(3):
+            for i in range(nmax):
+                for j in range(nmax):
+                    Qab[a,b] += 3*lab[a,i,j]*lab[b,i,j] - delta[a,b]
+    Qab = Qab/(2*nmax*nmax)
+    return Qab
+  
+  
 def get_order(arr,nmax):
     """
     Arguments:
@@ -221,13 +233,7 @@ def get_order(arr,nmax):
     # Generate a 3D unit vector for each cell (i,j) and
     # put it in a (3,i,j) array.
     #
-    lab = np.vstack((np.cos(arr),np.sin(arr),np.zeros_like(arr))).reshape(3,nmax,nmax)
-    for a in range(3):
-        for b in range(3):
-            for i in range(nmax):
-                for j in range(nmax):
-                    Qab[a,b] += 3*lab[a,i,j]*lab[b,i,j] - delta[a,b]
-    Qab = Qab/(2*nmax*nmax)
+    Qab = calc_order(Qab, delta, arr, nmax)
     eigenvalues,eigenvectors = np.linalg.eig(Qab)
     return eigenvalues.max()
   
