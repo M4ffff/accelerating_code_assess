581d4ab5bcdc6e3b0c9f81cffa43005e47d9400b
Matthew Parker <matthewparker0105@yahoo.com>
2025-02-17 10:58:01 +0000

    failed attempt to vectorise mc step - can't be vectorised because energies need to be calculated sequentially

--- NumpyVecLebwohlLasher.py
+++ NumpyVecLebwohlLasher.py
@@ -142,7 +142,7 @@ def savedat(arr,nsteps,Ts,runtime,ratio,energy,order,nmax):
     
     
 #=======================================================================
-def one_energy(arr,ix,iy,nmax):
+def one_energy_vectorised(arr):
     """
     Arguments:
 	  arr (float(nmax,nmax)) = array that contains lattice data;
@@ -157,11 +157,11 @@ def one_energy(arr,ix,iy,nmax):
 	Returns:
 	  en (float) = reduced energy of cell.
     """
-    en = 0.0
-    ixp = (ix+1)%nmax # These are the coordinates
-    ixm = (ix-1)%nmax # of the neighbours
-    iyp = (iy+1)%nmax # with wraparound
-    iym = (iy-1)%nmax #
+    en = np.zeros(arr.shape)
+    # ixp = (ix+1)%nmax # These are the coordinates
+    # ixm = (ix-1)%nmax # of the neighbours
+    # iyp = (iy+1)%nmax # with wraparound
+    # iym = (iy-1)%nmax #
 #
 # Add together the 4 neighbour contributions
 # to the energy
@@ -169,19 +169,64 @@ def one_energy(arr,ix,iy,nmax):
 
 # HERE
 # PARALLELISE
-    ang = arr[ix,iy]-arr[ixp,iy]
-    en += 0.5*(1.0 - 3.0*np.cos(ang)**2)
-    ang = arr[ix,iy]-arr[ixm,iy]
-    en += 0.5*(1.0 - 3.0*np.cos(ang)**2)
-    ang = arr[ix,iy]-arr[ix,iyp]
-    en += 0.5*(1.0 - 3.0*np.cos(ang)**2)
-    ang = arr[ix,iy]-arr[ix,iym]
-    en += 0.5*(1.0 - 3.0*np.cos(ang)**2)
+    ang1 = arr - np.roll(arr, -1, axis=1)
+    en += ( 0.5*(1.0 - 3.0*np.cos(ang1)**2) )
+    ang2 = arr - np.roll(arr, 1, axis=1)
+    en += ( 0.5*(1.0 - 3.0*np.cos(ang2)**2) )
+    ang3 = arr - np.roll(arr, -1, axis=0)
+    en += ( 0.5*(1.0 - 3.0*np.cos(ang3)**2) )
+    ang4 = arr - np.roll(arr, 1, axis=0)
+    en += ( 0.5*(1.0 - 3.0*np.cos(ang4)**2) )
     return en
+    
+    
+  
+#=======================================================================
+def one_energy(arr,ix,iy,nmax):
+    """
+    Arguments:
+	  arr (float(nmax,nmax)) = array that contains lattice data;
+	  ix (int) = x lattice coordinate of cell;
+	  iy (int) = y lattice coordinate of cell;
+      nmax (int) = side length of square lattice.
+    Description:
+      Function that computes the energy of a single cell of the
+      lattice taking into account periodic boundaries.  Working with
+      reduced energy (U/epsilon), equivalent to setting epsilon=1 in
+      equation (1) in the project notes.
+	Returns:
+	  energy (float) = reduced energy of cell.
+    """
+    energy = 0.0
+    
+    # adjacent in x direction (right/left)
+    ixr = (ix+1)%nmax # These are the coordinates of the neighbours (including wrapped around)
+    ixl = (ix-1)%nmax #  
+    
+    # adjacent in y direction (up/down)
+    iyu = (iy+1)%nmax # 
+    iyd = (iy-1)%nmax #
+#
+# Add together the 4 neighbour contributions
+# to the energy
+#
+
+# HERE
+# PARALLELISE
+    # 
+    ang = arr[ix,iy]-arr[ixr,iy]
+    energy += 0.5*(1.0 - 3.0*np.cos(ang)**2)
+    ang = arr[ix,iy]-arr[ixl,iy]
+    energy += 0.5*(1.0 - 3.0*np.cos(ang)**2)
+    ang = arr[ix,iy]-arr[ix,iyu]
+    energy += 0.5*(1.0 - 3.0*np.cos(ang)**2)
+    ang = arr[ix,iy]-arr[ix,iyd]
+    energy += 0.5*(1.0 - 3.0*np.cos(ang)**2)
+    return energy
   
   
 #=======================================================================
-def all_energy(arr,nmax):
+def all_energy_vectorised(arr):
     """
     Arguments:
 	  arr (float(nmax,nmax)) = array that contains lattice data;
@@ -192,12 +237,11 @@ def all_energy(arr,nmax):
 	Returns:
 	  enall (float) = reduced energy of lattice.
     """
-    enall = 0.0
-    for i in range(nmax):
-        for j in range(nmax):
-            enall += one_energy(arr,i,j,nmax)
-    return enall
+    # enall = 0.0
+    # enall = np.sum(one_energy_vectorised(arr))
+    return np.sum(one_energy_vectorised(arr))
   
+
   
 #=======================================================================
 def get_order(arr,nmax):
@@ -246,36 +290,34 @@ def MC_step(arr,Ts,nmax):
 	Returns:
 	  accept/(nmax**2) (float) = acceptance ratio for current MCS.
     """
-    #
-    # Pre-compute some random numbers.  This is faster than
-    # using lots of individual calls.  "scale" sets the width
-    # of the distribution for the angle changes - increases
-    # with temperature.
+   
+    # std
     scale=0.1+Ts
-    accept = 0
-    xran = np.random.randint(0,high=nmax, size=(nmax,nmax))
-    yran = np.random.randint(0,high=nmax, size=(nmax,nmax))
-    aran = np.random.normal(scale=scale, size=(nmax,nmax))
-    for i in range(nmax):
-        for j in range(nmax):
-            ix = xran[i,j]
-            iy = yran[i,j]
-            ang = aran[i,j]
-            en0 = one_energy(arr,ix,iy,nmax)
-            arr[ix,iy] += ang
-            en1 = one_energy(arr,ix,iy,nmax)
-            if en1<=en0:
-                accept += 1
-            else:
-            # Now apply the Monte Carlo test - compare
-            # exp( -(E_new - E_old) / T* ) >= rand(0,1)
-                boltz = np.exp( -(en1 - en0) / Ts )
 
-                if boltz >= np.random.uniform(0.0,1.0):
-                    accept += 1
-                else:
-                    arr[ix,iy] -= ang
-    return accept/(nmax*nmax)
+    # angles
+    aran = np.random.normal(scale=scale, size=(nmax,nmax))
+    
+    # DO EVERY SAMPLE AT ONCE
+    print(arr)
+    print(aran)
+    en0 = one_energy_vectorised(arr)
+    arr += aran
+    en1 = one_energy_vectorised(arr)
+
+    diff = (en1 - en0)
+    
+    rand_arr = np.random.uniform(0.0, 1.0, (diff.shape))
+    boltz = np.exp( -(diff) / Ts )
+    
+    accepted = (diff <= 0) | ( (diff > 0) & (boltz > rand_arr) )
+    
+    num_accepted = np.sum(accepted)
+    print(f"accepted boltz values: {num_accepted}")
+    
+    arr = np.where(accepted, arr, arr-aran)  
+    print(arr)
+    print("############")
+    return num_accepted/(nmax*nmax)
   
   
 #=======================================================================
@@ -301,7 +343,7 @@ def main(program, nsteps, nmax, temp, pflag):
     ratio = np.zeros(nsteps+1,dtype=np.float64)
     order = np.zeros(nsteps+1,dtype=np.float64)
     # Set initial values in arrays
-    energy[0] = all_energy(lattice,nmax)
+    energy[0] = all_energy_vectorised(lattice)
     ratio[0] = 0.5 # ideal value
     order[0] = get_order(lattice,nmax)
 
@@ -309,7 +351,7 @@ def main(program, nsteps, nmax, temp, pflag):
     initial = time.time()
     for it in range(1,nsteps+1):
         ratio[it] = MC_step(lattice,temp,nmax)
-        energy[it] = all_energy(lattice,nmax)
+        energy[it] = all_energy_vectorised(lattice)
         order[it] = get_order(lattice,nmax)
     final = time.time()
     runtime = final-initial
@@ -317,7 +359,7 @@ def main(program, nsteps, nmax, temp, pflag):
     # Final outputs
     print("{}: Size: {:d}, Steps: {:d}, T*: {:5.3f}: Order: {:5.3f}, Time: {:8.6f} s".format(program, nmax,nsteps,temp,order[nsteps-1],runtime))
     # Plot final frame of lattice and generate output file
-    savedat(lattice,nsteps,temp,runtime,ratio,energy,order,nmax)
+    # savedat(lattice,nsteps,temp,runtime,ratio,energy,order,nmax)
     plotdat(lattice,pflag,nmax)
     
     
